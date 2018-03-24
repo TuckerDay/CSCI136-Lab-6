ec63d3e80c4b6143d3ad9eb4409655b1c8d84bef
TuckerDay <37190942+TuckerDay@users.noreply.github.com>
2018-03-24 02:20:23 -0600

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,29 +1,35 @@
 //***************************************************************
 //Author: Tucker Day
 //Partner: Catelyn Meador
-//File: Enemy.java
+//File: Player.java
 //
-//Purpose: Enemy class for lab 6 game
+//Purpose: PLayer class
 //Last Changed Date: 3/10/18
 //***************************************************************
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Player {
+public class Player implements KeyListener {
 	
 	// Attributes
 	private String playerImage;
-	//private int health;
 	private int xPos;
 	private int yPos;
-	//private int numItems;
+	
+	GamePanel myPanel;
+	
 	
 	// Constructor
-	public Player(String playerImage, int xPos, int yPos)
+	public Player(String playerImage, int xPos, int yPos, GamePanel myPanel)
 	{
 		this.playerImage = playerImage;
-		//this.health = health;
 		this.xPos = xPos;
 		this.yPos = yPos;
-		//this.numItems = numItems;
+		
+		// Refers to panel
+		this.myPanel = myPanel;
+		myPanel.setFocusable(true);
+		myPanel.addKeyListener(this);
 	}
 	
 	// Methods
@@ -41,11 +47,6 @@ public class Player {
 		return playerImage;
 	}
 	
-	/*public int getHealth()
-	{
-		// returns health
-	}*/
-	
 	public int getX() {
 		return xPos;
 	}
@@ -54,20 +55,42 @@ public class Player {
 		return yPos;
 	}
 	
-	/*public int getNumItems()
-	{
-		// gets number of items
-	}*/
-	
-	/*public int setNumItems()
-	{
-		// sets number of items
-	}*/
-	
-	/*public int setHealth()
-	{
-		//sets health
-	}*/
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		if(arg0.getKeyCode() == KeyEvent.VK_LEFT)
+		{
+			setX(getX()-10);
+		}
+		
+		else if(arg0.getKeyCode() == KeyEvent.VK_RIGHT)
+		{
+			setX(getX()+10);
+		}
+		
+		else if(arg0.getKeyCode() == KeyEvent.VK_UP)
+		{
+			setY(getY()-10);
+		}
+		
+		else if(arg0.getKeyCode() == KeyEvent.VK_DOWN)
+		{
+			setY(getY()+10);
+		}
+		
+	myPanel.repaint();
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 
 }
